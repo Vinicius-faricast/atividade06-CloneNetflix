7de7971081639bc6829230d5f2b9c024d4645f48
Vinicius <vfarias.designer@gmail.com>
2024-08-24 14:03:30 -0300

    first configs

---
+++ clone-netflix/src/App.jsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./assets/styles/GobalStyles"
+
+function App() {
+
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
